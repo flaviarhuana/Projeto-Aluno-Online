1d86633cc6b06c98f866922884ff2e7c14141858
Flávia Rhuana <flaviarhuana.quim@gmail.com>
2026-06-02 16:06:45 -0300

    PATCH no controller

--- src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -1,5 +1,6 @@
 package br.com.alunoonline.api.controller;
 
+import br.com.alunoonline.api.dtos.AtualizarNotasRequestDTO;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.service.MatriculaAlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,12 @@ public class MatriculaAlunoController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void trancarMatricula(@PathVariable Long id){
         matriculaAlunoService.trancarMatricula(id);
+    }
+
+    @PatchMapping("/notas/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void atualizarNotas(@PathVariable Long id, @RequestBody AtualizarNotasRequestDTO dto) {
+        matriculaAlunoService.atualizarNotas(id, dto);
     }
 
     @DeleteMapping("/{id}")
